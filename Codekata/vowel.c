7b8bbb165304baf4a054129a18a4e02accee2ed3
17UITE027 <38652903+17UITE027@users.noreply.github.com>
2018-06-19 11:06:16 +0000

    Create vowel.c

---
+++ Codekata/vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  char str;
+  scanf("%s",a);
+  if(str=='A'||str=='E'||str=='I'||str=='O'||str=='U'||str=='a'||str=='e'||str=='u'||str=='i'||str=='o')
+  {
+    printf("Vowel");
+  }
+  else
+  {
+    printf("Consonent");
+  }
+  return 0;
+}
