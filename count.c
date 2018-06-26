fba77a04e44b819b69b0a59d43675141c609a19a
17UITE027 <38652903+17UITE027@users.noreply.github.com>
2018-06-26 16:11:48 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n,count=0,rem;
+  scanf("%d",&n);
+  for(i=o;n>0;i++)
+  {
+    rem=n%10;
+    n=n/10;
+    count=count+1;
+   }
+   printf("%d",count);
+   return 0;
+}
