ea3ff1ddd1e63f4b3f3e017b317281d335e7251a
17UITE027 <38652903+17UITE027@users.noreply.github.com>
2018-06-30 07:23:18 +0000

    Update prime.c

--- prime.c
+++ prime.c
@@ -5,4 +5,21 @@ int main()
   scanf("%d",&N);
   if(N<=1000)
   {
-  
+    for(i=2;i<=N/2;i++)
+    {
+      if(N%i==0)
+      {
+        flag=1;
+        break;
+      }
+    }
+    if (flag==1)
+    {
+      printf("no");
+    }
+    else
+    {
+      printf("yes");
+    }
+ return 0;
+  }
